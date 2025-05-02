79821288c5a0c23ce053dba1813ac873a5da7def
{Vyacheslav} <{pechislap@gmail.com}>
2025-05-02 19:23:14 +0300

    дз 10

--- src/main/java/ru/otus/java/basic/homeworks/homework10/User.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/User.java
@@ -4,6 +4,6 @@ public class User {
     private String surname;
     private String name;
     private String patronymic;
-    private Integer age;
+    private int age;
     private String email;
 }
