f73b2d6cfaab00fefb80c1989d90944a539bf7b2
{Vyacheslav} <{pechislap@gmail.com}>
2025-05-02 19:21:03 +0300

    дз 10

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/Homework10.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks.homework10;
+
+public class Homework10 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/User.java
@@ -0,0 +1,9 @@
+package ru.otus.java.basic.homeworks.homework10;
+
+public class User {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private Integer age;
+    private String email;
+}
