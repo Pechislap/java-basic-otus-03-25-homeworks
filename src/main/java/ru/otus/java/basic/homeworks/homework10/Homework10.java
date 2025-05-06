0bd69978b9fd7ab18077304bd8b67cd03cc3a310
{Vyacheslav} <{pechislap@gmail.com}>
2025-05-06 15:11:33 +0300

    дз 10

--- src/main/java/ru/otus/java/basic/homeworks/homework10/Homework10.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/Homework10.java
@@ -2,6 +2,19 @@ package ru.otus.java.basic.homeworks.homework10;
 
 public class Homework10 {
     public static void main(String[] args) {
+        User user = new User("Иванов", "Иван", "Иванович", 1986, "ivanov@mail.ru");
+        User user1 = new User("Петров", "Пётр", "Петрович", 2003, "petrov@mail.ru");
+        User user2 = new User("Сидоров", "Сидор", "Сидорович", 1974, "sidorov@mail.ru");
+        User user3 = new User("Дементьев", "Дементий", "Дементьевич", 2008, "dementev@mai.ru");
+        User user4 = new User("Голубев", "Сергей", "Петрович", 1996, "golubev@mail.ru");
+        // new User("Голубев", "Сергей", "Петрович", 1996, "golubev@mail.ru"),
+        //new User("Зафаров", "Зафар", "Зафарович", 2000, "zafarov@mail.ru"),
+        //new User("Ким", "Вячеслав", "Константинович", 1966, "pechislap@gmail.com"),
+        //new User("Ломоносов", "Михайло", "Васильевич", 1711, "lomonosov@mail.ru"),
+        //new User("Наумов", "Сергей", "Борисович", 2005, "naumov@mail.ru"),
+        //new User("Филонов", "Сергей", "Александрович", 1984, "filonov@mai.ru")
+
+
 
     }
 }
--- src/main/java/ru/otus/java/basic/homeworks/homework10/User.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/User.java
@@ -4,6 +4,61 @@ public class User {
     private String surname;
     private String name;
     private String patronymic;
-    private int age;
+    private int birthYear;
     private String email;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int age) {
+        this.birthYear = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String surname, String name, String patronymic, int birthYear, String email) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.birthYear = birthYear;
+        this.email = email;
+    }
+
+    public void Info() {
+        System.out.println("ФИО: " + surname + " " + name + " " + patronymic);
+        System.out.println("Гож рождения: " + birthYear);
+        System.out.println("email: " + email);
+    }
 }
+
