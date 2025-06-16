fa4357594656e32d60bdd97c23a6178020ffb061
{Vyacheslav} <{pechislap@gmail.com}>
2025-06-16 12:08:46 +0300

    дз 17

--- src/main/java/ru/otus/java/basic/homeworks/homework17/PhoneBook.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework17/PhoneBook.java
@@ -2,17 +2,19 @@ package ru.otus.java.basic.homeworks.homework17;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class PhoneBook {
-    private HashMap<String, HashSet<String>> hmPhoneBook;
+    private final Map<String, Set<String>> hmPhoneBook;
 
     public PhoneBook() {
         hmPhoneBook = new HashMap<>();
     }
 
     public void add(String fio, String phoneNumber) {
-        if (!hmPhoneBook.containsKey(fio)) {
+
+         if (!hmPhoneBook.containsKey(fio)){
             hmPhoneBook.put(fio, new HashSet<>());
         }
         hmPhoneBook.get(fio).add(phoneNumber);
